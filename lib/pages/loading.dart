ac68c31627e2bfab26b60379abcc557fc171d350
mandeep111 <mandeepdhakal11@gmail.com>
2020-04-18 16:44:52 +0545

    api request

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldtime/service/WorldTime.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +7,26 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  String time = "loading";
+
+  void setupWorldTIme() async {
+    WorldTime instance = WorldTime(location: 'Berlin', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTIme();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading'),
+      body: Text(time),
     );
   }
 }
